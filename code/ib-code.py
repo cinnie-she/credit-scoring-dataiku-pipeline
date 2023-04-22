3f5c8a0955e15da1b0a84edaa3d051e8ac4a3e3d
cinnie-she <100207222+cinnie-she@users.noreply.github.com>
2023-04-22 22:33:47 +0800

    convert ab bin to custom bin def format

--- code/ib-code.py
+++ code/ib-code.py
@@ -566,7 +566,7 @@ class StatCalculator:
         col_df = self.df.loc[:, [col_to_bin]]  # get a single column
 
         # perform binning
-        binned_series = BinningMachine.perform_binning_on_col(
+        _, binned_series = BinningMachine.perform_binning_on_col(
             col_df, self.col_bins_settings)
         self.df.insert(loc=0, column=col_to_bin+"_binned", value=binned_series)
 
@@ -843,23 +843,38 @@ def generate_mixed_chart_fig(
 
     return fig
 
+
+def get_str_from_ranges(ranges):
+        if not isinstance(ranges, list):
+            return -1
+        if len(ranges) == 0:
+            return '[]'
+        
+        ranges = GoodBadDefDecoder.sort_numerical_def_ranges(ranges)
+        
+        ranges_str = "["
+        ranges_str = f"{ranges_str}[{ranges[0][0]}, {ranges[0][1]})"
+        for idx in range(1, len(ranges)):
+            ranges_str = f"{ranges_str}, [{ranges[idx][0]}, {ranges[idx][1]})"
+        ranges_str += "]"
+        return ranges_str  
+
+
 # A class for performing binning based on bins settings
-
-
 class BinningMachine:
     # Perform equal width binning based on a specified width (for numerical column only)
     @staticmethod
     def perform_eq_width_binning_by_width(col_df, width):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
         if col_df.isna().all().all():
-            return pd.Series([None for _ in range(len(col_df))])
+            return (-1, pd.Series([None for _ in range(len(col_df))]))
         # Cannot be categorical type
         if not pd.api.types.is_numeric_dtype(col_df.iloc[:, 0]):
-            return -1
+            return (-1, -1)
         # width cannot be non-numeric
         if not (isinstance(width, int) or isinstance(width, float)) or width <= 0:
-            return -1
+            return (-1, -1)
 
         min = col_df.min()
         max = col_df.max()
@@ -883,20 +898,25 @@ class BinningMachine:
                 if val >= bin_range[0] and val < bin_range[1]:
                     binned_result.append(f"[{bin_range[0]}, {bin_range[1]})")
                     break
-        return pd.Series(binned_result)
+        
+        def_li = list()
+        for r in bin_ranges:
+            def_li.append({"name": get_str_from_ranges([r]), "ranges": [r]})
+        
+        return (def_li, pd.Series(binned_result))
 
     # A method to perform equal width binning based on a specified number of bins
     @staticmethod
     def perform_eq_width_binning_by_num_bins(col_df, num_bins):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
         if col_df.isna().all().all():
-            return pd.Series([None for _ in range(len(col_df))])
+            return (-1, pd.Series([None for _ in range(len(col_df))]))
         # Cannot be categorical type
         if not pd.api.types.is_numeric_dtype(col_df.iloc[:, 0]):
-            return -1
+            return (-1, -1)
         if not isinstance(num_bins, int) or num_bins <= 0:
-            return -1
+            return (-1, -1)
 
         min = col_df.min()
         max = col_df.max()
@@ -922,20 +942,25 @@ class BinningMachine:
                 if val >= bin_range[0] and val < bin_range[1]:
                     binned_result.append(f"[{bin_range[0]}, {bin_range[1]})")
                     break
-        return pd.Series(binned_result)
+                
+        def_li = list()
+        for r in bin_ranges:
+            def_li.append({"name": get_str_from_ranges([r]), "ranges": [r]})
+        
+        return (def_li, pd.Series(binned_result))
 
     # A method to perform equal frequency binning based on a specified frequency
     @staticmethod
     def perform_eq_freq_binning_by_freq(col_df, freq):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
         if col_df.isna().all().all():
-            return pd.Series([None for _ in range(len(col_df))])
+            return (-1, pd.Series([None for _ in range(len(col_df))]))
         # Cannot be categorical type
         if not pd.api.types.is_numeric_dtype(col_df.iloc[:, 0]):
-            return -1
+            return (-1, -1)
         if not isinstance(freq, int) or freq <= 0 or freq > len(col_df):
-            return -1
+            return (-1, -1)
 
         num_bins = int(np.ceil(len(col_df)/freq))
         # print(num_bins)
@@ -960,20 +985,30 @@ class BinningMachine:
                 binned_result.append(
                     f"[{interval_li[idx].left}, {interval_li[idx].right})")
 
-        return pd.Series(binned_result)
+        def_set = set()
+        for idx in range(len(interval_li)):
+            if isinstance(interval_li[idx], pd._libs.interval.Interval):
+                def_set.add((interval_li[idx].left, interval_li[idx].right))
+        bin_ranges = list(def_set)
+        
+        def_li = list()
+        for r in bin_ranges:
+            def_li.append({"name": get_str_from_ranges([[r[0], r[1]]]), "ranges": [[r[0], r[1]]]})
+        
+        return (def_li, pd.Series(binned_result))
 
     # A method to perform equal-frequency binning based on a specified number of bins
     @staticmethod
     def perform_eq_freq_binning_by_num_bins(col_df, num_bins):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
         if col_df.isna().all().all():
-            return pd.Series([None for _ in range(len(col_df))])
+            return (-1, pd.Series([None for _ in range(len(col_df))]))
         # Cannot be categorical type
         if not pd.api.types.is_numeric_dtype(col_df.iloc[:, 0]):
-            return -1
+            return (-1, -1)
         if not isinstance(num_bins, int) or num_bins <= 0:
-            return -1
+            return (-1, -1)
 
         interval_li = pd.qcut(
             col_df.iloc[:, 0], num_bins, duplicates="drop").to_list()
@@ -991,13 +1026,23 @@ class BinningMachine:
                 binned_result.append(
                     f"[{interval_li[idx].left}, {interval_li[idx].right})")
 
-        return pd.Series(binned_result)
+        def_set = set()
+        for idx in range(len(interval_li)):
+            if isinstance(interval_li[idx], pd._libs.interval.Interval):
+                def_set.add((interval_li[idx].left, interval_li[idx].right))
+        bin_ranges = list(def_set)
+        
+        def_li = list()
+        for r in bin_ranges:
+            def_li.append({"name": get_str_from_ranges([[r[0], r[1]]]), "ranges": [[r[0], r[1]]]})
+        
+        return (def_li, pd.Series(binned_result))
 
     # A method to perform custom binning for a categorical column
     @staticmethod
     def perform_categorical_custom_binning(col_df, bins_settings):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
 
         binned_result = list()
 
@@ -1012,13 +1057,13 @@ class BinningMachine:
             if has_assigned_bin == False:  # does not belongs to any bin
                 binned_result.append(None)
 
-        return pd.Series(binned_result)
+        return (bins_settings, pd.Series(binned_result))
 
     # A method to perform custom binning for a numerical column
     @staticmethod
     def perform_numerical_custom_binning(col_df, bins_settings):
         if len(col_df) == 0:
-            return -1
+            return (-1, -1)
 
         binned_result = list()
 
@@ -1035,7 +1080,7 @@ class BinningMachine:
             if has_assigned_bin == False:  # does not belongs to any bin
                 binned_result.append(None)
 
-        return pd.Series(binned_result)
+        return (bins_settings, pd.Series(binned_result))
 
     # A method to perform binning (equal-width/equal-frequency/custom) for a single column (either categorical or numerical)
     @staticmethod
@@ -1060,31 +1105,40 @@ class BinningMachine:
         """
         if col_bins_settings["bins"] == "none":
             if len(col_df) == 0:
-                return -1
-            return col_df.iloc[:, 0]  # no binning
+                return (-1, -1)
+            unique_bin = col_df.iloc[:, 0].unique().tolist()
+            if col_bins_settings["type"] == "numerical":
+                def_li = list()
+                for bin in unique_bin:
+                    def_li.append({"name": get_str_from_ranges([[bin, bin+1]]), "ranges": [[bin, bin+1]]})
+            else:
+                def_li = list()
+                for bin in unique_bin:
+                    def_li.append({"name": str([bin]), "elements": [bin]})
+            return (def_li, col_df.iloc[:, 0])  # no binning
         elif isinstance(col_bins_settings["bins"], dict):  # auto binning
             if col_bins_settings["bins"]["algo"] == "equal width":
                 if col_bins_settings["bins"]["method"] == "width":
                     if col_bins_settings["type"] == "numerical":
                         return BinningMachine.perform_eq_width_binning_by_width(col_df, col_bins_settings["bins"]["value"])
                     else:
-                        return -1
+                        return (-1, -1)
                 else:  # by num of bins
                     if col_bins_settings["type"] == "numerical":
                         return BinningMachine.perform_eq_width_binning_by_num_bins(col_df, col_bins_settings["bins"]["value"])
                     else:
-                        return -1
+                        return (-1, -1)
             else:  # equal frequency
                 if col_bins_settings["bins"]["method"] == "freq":
                     if col_bins_settings["type"] == "numerical":
                         return BinningMachine.perform_eq_freq_binning_by_freq(col_df, col_bins_settings["bins"]["value"])
                     else:
-                        return -1
+                        return (-1, -1)
                 else:  # by num of bins
                     if col_bins_settings["type"] == "numerical":
                         return BinningMachine.perform_eq_freq_binning_by_num_bins(col_df, col_bins_settings["bins"]["value"])
                     else:
-                        return -1
+                        return (-1, -1)
         else:  # custom binning
             if col_bins_settings["type"] == "numerical":
                 return BinningMachine.perform_numerical_custom_binning(col_df, col_bins_settings["bins"])
@@ -1111,7 +1165,7 @@ class BinningMachine:
             if col_bins_settings == None:
                 continue
 
-            binned_series = BinningMachine.perform_binning_on_col(
+            _, binned_series = BinningMachine.perform_binning_on_col(
                 col_df, col_bins_settings)
             if not isinstance(binned_series, pd.Series):  # error occurs
                 return -1
@@ -3226,7 +3280,10 @@ refresh button in automated binning panel
 
 
 @app.callback(
-    Output("temp_col_bins_settings", "data"),
+    [
+        Output("temp_col_bins_settings", "data"),
+        Output("temp_binned_col", "data"),
+    ],
     [
         Input("predictor_var_ib_dropdown", "value"),
         Input("auto_bin_refresh_button", "n_clicks"),
@@ -3283,31 +3340,17 @@ def update_temp_bins_settings(var_to_bin, n_clicks, bins_settings_data, auto_bin
         else:  # none
             col_bins_settings["bins"] = "none"
 
-    return json.dumps(col_bins_settings)
-
-
-"""
-Interactive Binning Page:
-Update temp_binned_col whenever temp_col_bins_settings
-has changed
-"""
-
-
-@app.callback(
-    Output("temp_binned_col", "data"),
-    Input("temp_col_bins_settings", "data"),
-)
-def update_temp_binned_col(temp_col_bins_settings_data):
-    triggered = dash.callback_context.triggered
-
-    col_bins_settings = json.loads(temp_col_bins_settings_data)
-
-    binned_series = BinningMachine.perform_binning_on_col(
+    def_li, binned_series = BinningMachine.perform_binning_on_col(
         df.loc[:, [col_bins_settings["column"]]], col_bins_settings)
     temp_df = df.copy()
     temp_df['binned_col'] = binned_series.values
+    
+    col_bins_settings["bins"] = def_li
+        
+    return [json.dumps(col_bins_settings), json.dumps(temp_df.to_dict())]
 
-    return json.dumps(temp_df.to_dict())
+
+
 
 
 """
